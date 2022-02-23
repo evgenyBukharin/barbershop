58f39458cd2175a82a27efa5ad0b54adc05f5f4e
Александр Бухарин <buharinzxc123@gmail.com>
2022-02-23 18:11:11 +0500

    404

--- 404.php
+++ 404.php
@@ -1,3 +1,12 @@
+<?php require_once('php/header.php'); ?>
+
+<link rel="stylesheet" href="/css/global.css">
+<link rel="stylesheet" href="/css/header.css">
+<link rel="stylesheet" href="/css/card.css">
+<link rel="stylesheet" href="/css/login.css">
+
 <title>404 | Страница не существует</title>
-<h1>ERROR 404</h1>
-<h2>Страница не существует!</h2>
+<div class="container mt-12">
+    <h1>ERROR 404</h1>
+    <h2 class="mt-12">Страница не существует!</h2>
+</div>
