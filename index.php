0eb818e97b978d3cefede51c9565174e84172a7e
Александр Бухарин <buharinzxc123@gmail.com>
2022-06-16 21:14:37 +0500

    +index map +service price

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require_once('php/scripts/db.php'); ?>
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,18 +11,22 @@
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/header.css">
 </head>
+
 <body>
     <?php
-        require_once('php/header.php')
+    require_once('php/header.php')
     ?>
     <div class="flex jc-center ai-center min-h-100">
         <div class="container container__fix">
             <h1 class="main__title">Мы - профессиональный барбершоп с лучшими мастерами</h1>
             <span class="custom-hr"></span>
             <h2 class="main__title_sub">Множество довольных клиентов и отзывов</h2>
-            <button class="btn-reset btn-primary" id="service-link">Просмотреть услуги</button>
+            <button class="btn-reset btn-primary main__btn" id="service-link">Просмотреть услуги</button>
+            <h2 class="main__title_address">Вы можете найти нас по адресу: улица Ворошилова, 57.</h2>
+            <img src="images/map.png" alt="Изображение карты" class="main__map">
         </div>
     </div>
-    <script src="js/index.js"></script>  
+    <script src="js/index.js"></script>
 </body>
+
 </html>
--- php/pages/service.php
+++ php/pages/service.php
@@ -10,28 +10,29 @@
 <?php require_once('./../header.php'); ?>
 
 <div class="container mb-4">
-    <?php 
-        $sql = "SELECT * FROM `service`";
-        $query = $connect -> prepare($sql);
-        $query -> execute();
-        $result = $query -> fetchAll(PDO::FETCH_ASSOC);
-        echo '<div class="card__container">';
-        foreach ($result as $key => $value) {
-            echo 
-            '
+    <?php
+    $sql = "SELECT * FROM `service`";
+    $query = $connect->prepare($sql);
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo '<div class="card__container">';
+    foreach ($result as $key => $value) {
+        echo
+        '
                 <form class="card">
                     <div>
                         <h3 class="card__title">' . $result[$key]["title"] . '</h3>
                         <p class="card__description">' . $result[$key]["description"] . '</p>
+                        <h3 class="mt-12">Цена услуги: ' . $result[$key]["price"] . ' руб.</h3>
                     </div>
                     <div style="width: 100%" class="flex jc-center">
                         <img class="card__image" src="' . $result[$key]["img"] . '"/>
                     </div>
                 </form>
             ';
-        }
-        echo '</div>';
-    ?>    
+    }
+    echo '</div>';
+    ?>
 </div>
 
 <script src="/js/global.js"></script>
